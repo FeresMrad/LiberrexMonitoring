eba31bd6badd54925341807f0d529241208623f6
FeresMrad <mohamedferes.mrad@esprit.tn>
2025-04-03 09:32:27 +0100

    added system boot to metricsview / changed specstable layout

--- src/components/SpecsTable.vue
+++ src/components/SpecsTable.vue
@@ -3,25 +3,26 @@
       <table>
         <thead>
           <tr>
-            <th>Type</th>
-            <th>Current</th>
-            <th>Max</th>
+            <th>Resource</th>
+            <th>Value</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Memory (GB)</td>
-            <td>{{ memoryCurrent.toFixed(2) }}</td>
-            <td>{{ memoryMax.toFixed(2) }}</td>
+            <td>Memory</td>
+            <td>{{ memoryCurrent.toFixed(1) }}/{{ memoryMax.toFixed(1) }} GB</td>
           </tr>
           <tr>
-            <td>Disk (GB)</td>
-            <td>{{ diskCurrent.toFixed(2) }}</td>
-            <td>{{ diskMax.toFixed(2) }}</td>
+            <td>Disk</td>
+            <td>{{ diskCurrent.toFixed(1) }}/{{ diskMax.toFixed(1) }} GB</td>
           </tr>
           <tr>
             <td>IP Address</td>
             <td>{{ ipAddress }}</td>
+          </tr>
+          <tr>
+            <td>System Boot</td>
+            <td>{{ uptime }}</td>
           </tr>
         </tbody>
       </table>
@@ -44,34 +45,45 @@
   const diskCurrent = ref(0);
   const diskMax = ref(0);
   const ipAddress = ref(0);
+  const uptime = ref(0);
   
   const fetchData = async () => {
     try {
-      const memoryQuery = `SELECT total-available, total FROM memory WHERE host='${props.host}' order by time desc limit 1`;
-      const diskQuery = `SELECT used, total FROM disk WHERE host='${props.host}' order by time desc limit 1`;
-      const ipAddressQuery = `Select ip_adr from network where host='${props.host}' order by time desc limit 1`;
-      
-      const memoryResponse = await axios.get("http://82.165.230.7:8086/query", {
-        params: { db: "metrics", q: memoryQuery, u: "liberrex", p: "test" }
-      });
-      
-      const diskResponse = await axios.get("http://82.165.230.7:8086/query", {
-        params: { db: "metrics", q: diskQuery, u: "liberrex", p: "test" }
-      });
+        const memoryQuery = `SELECT total-available, total FROM memory WHERE host='${props.host}' order by time desc limit 1`;
+        const diskQuery = `SELECT used, total FROM disk WHERE host='${props.host}' order by time desc limit 1`;
+        const ipAddressQuery = `SELECT ip_adr FROM network WHERE host='${props.host}' order by time desc limit 1`;
+        const uptimeQuery = `SELECT uptime_seconds FROM uptime WHERE host='${props.host}' order by time desc limit 1`;
+        
+        const responses = await Promise.all([
+            axios.get("http://82.165.230.7:8086/query", { params: { db: "metrics", q: memoryQuery, u: "liberrex", p: "test" } }),
+            axios.get("http://82.165.230.7:8086/query", { params: { db: "metrics", q: diskQuery, u: "liberrex", p: "test" } }),
+            axios.get("http://82.165.230.7:8086/query", { params: { db: "metrics", q: ipAddressQuery, u: "liberrex", p: "test" } }),
+            axios.get("http://82.165.230.7:8086/query", { params: { db: "metrics", q: uptimeQuery, u: "liberrex", p: "test" } })
+        ]);
 
-      const ipResponse = await axios.get("http://82.165.230.7:8086/query", {
-        params: { db: "metrics", q: ipAddressQuery, u: "liberrex", p: "test" }
-      });
-  
-      memoryCurrent.value = (memoryResponse.data.results[0]?.series[0]?.values[0][1] || 0) / 1e9;
-      memoryMax.value = (memoryResponse.data.results[0]?.series[0]?.values[0][2] || 0) / 1e9;
-      diskCurrent.value = (diskResponse.data.results[0]?.series[0]?.values[0][1] || 0) / 1e9;
-      diskMax.value = (diskResponse.data.results[0]?.series[0]?.values[0][2] || 0) / 1e9;
-      ipAddress.value = ipResponse.data.results[0]?.series[0]?.values?.[0]?.[1]
+        const memoryResponse = responses[0].data;
+        const diskResponse = responses[1].data;
+        const ipResponse = responses[2].data;
+        const uptimeResponse = responses[3].data;
+
+        memoryCurrent.value = (memoryResponse.results[0]?.series[0]?.values[0][1] || 0) / 1e9;
+        memoryMax.value = (memoryResponse.results[0]?.series[0]?.values[0][2] || 0) / 1e9;
+        diskCurrent.value = (diskResponse.results[0]?.series[0]?.values[0][1] || 0) / 1e9;
+        diskMax.value = (diskResponse.results[0]?.series[0]?.values[0][2] || 0) / 1e9;
+        ipAddress.value = ipResponse.results[0]?.series[0]?.values?.[0]?.[1];
+
+        const uptimeSeconds = uptimeResponse.results[0]?.series[0]?.values?.[0]?.[1] || 0;
+        
+        // Calculate system boot time
+        const bootTime = new Date(Date.now() - uptimeSeconds * 1000).toLocaleString("fr");
+
+        uptime.value = bootTime;
+
     } catch (error) {
-      console.error("Error fetching data:", error);
+        console.error("Error fetching data:", error);
     }
-  };
+};
+
   
   onMounted(() => {
     fetchData();
